966fa4fa0eed62349f6b278d875fb0fcd7d9a1dd
Aleksandr Shemetillo <aleksandr.pavlovich@gmail.com>
2017-05-12 17:55:13 +0300

    First branch (#1)
    * test commit
    
    test
    
    * master
    
    master
    
    * some troubles
    
    first branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-import Content from './Content/index'
-import Sidebar from './Sidebar/index'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+import ProductList from './Product-list/index';
+import ProductDetails from './Product-details/index';
 import './css/style.css';
-import './css/details.css';
-import './css/filter.css';
 import './css/grid.css';
 import './css/product-cards.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <Sidebar />
-        <Content />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={ProductDetails} />
+        </div>
+      </Router>
     );
   }
 }
