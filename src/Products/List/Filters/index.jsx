59b4d14270045410e7cbcb3b1644900c1abc7356
sapolio <aleksandr.pavlovich@gmail.com>
2017-05-16 11:59:46 +0300

    add some styled to filters and card

--- src/Products/List/Filters/index.jsx
+++ src/Products/List/Filters/index.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 import Button from './Button';
 import Label from './Label';
 
+const Filter = styled.div`
+  display: flex;
+  padding: 11px 0px 12px 25px;
+  font-family: AvenirNext-Bold;
+  border-bottom: #e7e7e7 3px solid;
+`;
+const GenderSelector = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 180px;
+`;
+const SizeSelector = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 323px;
+  margin-left: 51px;
+`;
 export default () => (
-  <div className="filter">
+  <Filter>
     <div className="filter-icon" />
 
-    <div className="filter-gender-select">
+    <GenderSelector>
       <Button className="filter-gender filter-active">MAN</Button>
       <Button className="filter-gender">WOMAN</Button>
-    </div>
+    </GenderSelector>
 
-    <div className="filter-size-select">
-      <Label>SIZE</Label>
+    <SizeSelector>
+      <Label active>SIZE</Label>
       <Button className="filter-size">36</Button>
       <Button className="filter-size">37</Button>
       <Button className="filter-size">38</Button>
@@ -20,6 +38,6 @@ export default () => (
       <Button className="filter-size">40</Button>
       <Button className="filter-size filter-active">41</Button>
       <Button className="filter-size">42</Button>
-    </div>
-  </div>
+    </SizeSelector>
+  </Filter>
 );
