9beb39c22850105b5111a554d0ed0337ca7a6642
sapolio <aleksandr.pavlovich@gmail.com>
2017-05-20 20:14:10 +0300

    dealing with eslint again

---
+++ src/Products/Wrapper.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  display: block;
+  margin: 0 auto;
+  height: 100%;
+  @media (min-width: 768px) {
+    display: flex;
+    max-width: 1440px;
+  }
+`;
+export default Wrapper;
